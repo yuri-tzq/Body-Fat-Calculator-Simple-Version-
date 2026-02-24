79dc5878d0215732f8382ddc3f23074faac226ff
Yuri <zhiqing.teoh@outlook.com>
2026-02-24 12:59:25 +0800

    Added some validation code

--- fat calculator.py
+++ fat calculator.py
@@ -8,26 +8,99 @@ while True:
     #if user is female
     if gender.lower() == "f":
 
-        #user's body data
-        height = float(input("Height (cm): ")) 
-        neck = float(input("Neck Circumference (cm): "))
-        waist = float(input("Waist Circumference (cm): "))
-        hip = float(input("Hip Circumference (cm): "))
+        #loop for height
+        while True:
+            #user's body data
+            height = float(input("Height (cm): ")) 
+
+            #check whether this mdfk's height is realistic or not
+            if height <= 0:
+                print("Height cannot be negative or zero.")
+            else:
+                break
+
+        #loop for neck
+        while True:
+            #user's body data
+            neck = float(input("Neck Circumference (cm): "))
+
+            #check whether this mdfk is realistic or not
+            if neck <= 0:
+                print("Neck cannot be negative or zero.")
+            else:
+                break
+
+        #loop for waist
+        while True:
+            #user's body data
+            waist = float(input("Waist Circumference (cm): "))
+
+            #check whether this mdfk is realistic or not
+            if waist <= 0:
+                print("waist cannot be negative or zero.")
+            else:
+                break
+
+        #loop for hip
+        while True:
+            hip = float(input("Hip Circumference (cm): "))
+
+            if hip <= 0:
+                print("Hip cannot be negative or zero.")
+            else:
+                break
         
-        fat = 163.205 * math.log10(waist + hip - neck) - 97.684 * math.log10(height) - 78.387
-        print(round(fat/2.54 , 2))
+        #calculation
+        fat = 163.205 * math.log10((waist + hip - neck)/2.54) - 97.684 * math.log10(height/2.54) - 78.387
+        print(f"Body Fat: {round(fat, 2)}%")
 
     #if user is male
     elif gender.lower() == "m":
 
-        #user's body data
-        height = float(input("Height (cm): "))
-        neck = float(input("Neck Circumference (cm): "))
-        waist = float(input("Waist Circumference (cm): "))
-        abdomen = float(input("Abdomen (cm): "))
+        #loop for height
+        while True:
+            #user's body data
+            height = float(input("Height (cm): ")) 
+
+            #check whether this mdfk's height is realistic or not
+            if height <= 0:
+                print("Height cannot be negative or zero.")
+            else:
+                break
+
+        #loop for neck
+        while True:
+            #user's body data
+            neck = float(input("Neck Circumference (cm): "))
+
+            #check whether this mdfk is realistic or not
+            if neck <= 0:
+                print("Neck cannot be negative or zero.")
+            else:
+                break
+
+        #loop for waist
+        while True:
+            #user's body data
+            waist = float(input("Waist Circumference (cm): "))
+
+            #check whether this mdfk is realistic or not
+            if waist <= 0:
+                print("waist cannot be negative or zero.")
+            else:
+                break
+
+        #loop for abdomen
+        while True:
+            abdomen = float(input("Hip Circumference (cm): "))
+
+            if abdomen <= 0:
+                print("Abdomen cannot be negative or zero.")
+            else:
+                break
         
-        fat = 86.010 * math.log10(abdomen - neck) - 70.041 * math.log10(height) + 36.76
-        print(round(fat/2.54 , 2))
+        fat = 86.010 * math.log10((abdomen - neck)/2.54) - 70.041 * math.log10(height/2.54) + 36.76
+        print(f"Body Fat: {round(fat, 2)}%")
 
     else:
-        print("I mean biological NOT your identity.")
+        print("Please type either 'M' or 'F' according to your biological gender.")
